765e0ba362daa6b83991736496bb94a5075b68aa
Khadija Iftikhar <khadijaiftikhar.azhar@gmail.com>
2024-02-05 13:27:58 +0500

    Remove User Validation

--- src/main/java/com/projects/banking/Validators/ValidUserRegister.java
+++
@@ -1,9 +0,0 @@
-package com.projects.banking.Validators;
-
-import jakarta.validation.Payload;
-
-public @interface ValidUserRegister {
-    String message() default "Field Must be Required.";
-    Class<?> [] groups() default {};
-    Class<? extends Payload> [] payload() default {};
-}
